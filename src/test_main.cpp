61006b4a7de45603991666b37e0dad85a048c09e
loresalm <lorenzo.salmina@epfl.ch>
2018-11-06 19:13:03 +0100

    almost ready

--- src/test_main.cpp
+++ src/test_main.cpp
@@ -2,9 +2,6 @@
 #include <algorithm>
 #include "network.h"
 #include "random.h"
-#include <iostream>
-
-using namespace std;
 
 RandomNumbers RNG(101);
 Network net;
@@ -26,15 +23,16 @@ TEST(networkTest, connect) {
     EXPECT_FALSE(trylink);
     trylink = net.add_link(9,10);
     EXPECT_TRUE(trylink);
-    cout <<"test netdeg"<<net.degree(9)<<"+"<< net.degree(10) << "= "<< net.degree(9)+net.degree(10)<<endl;
     EXPECT_EQ(net.degree(9)+net.degree(10), 2);    
     trylink = net.add_link(9,8);
     trylink = net.add_link(1,9);
     std::vector<size_t> ngb = net.neighbors(9);
     EXPECT_EQ(ngb.size(), 3);
     EXPECT_TRUE(ngb[0]==10 && ngb[1]==8 && ngb[2]==1);
-    size_t numlink = net.random_connect(2);
-    EXPECT_NEAR(numlink, 200, 5);
+    double numlink = 0;
+    for (int rep=0; rep<100; rep++) 
+      numlink += 0.01*net.random_connect(2);
+    EXPECT_NEAR(numlink, 200, 15);
 }
 
 TEST(networkTest, values) {
